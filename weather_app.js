aa420a647181f6a636ff6749720b8b5a64c0049f
dutta_ankit <ankit.dutta@itobuz.com>
2023-03-03 17:05:54 +0530

    Version 3.1

--- weather_app.js
+++ weather_app.js
@@ -1,17 +1,74 @@
-function getWeatherData(location) {
+// function getWeatherData(userLocation) {
+//     const weatherData =[
+//         {
+//             location: 'kolkata',
+//             lat:'23',
+//             long:'12',
+//             condition: {
+//                 current: 'sunny',
+//                 day_val: '1',
+//                 overcast: '9'
+
+//             }
+
+            
+//         }
+        
+//         ,{
+//             location: 'Mumbai',
+//             lat:'13',
+//             long:'42',
+//             condition: {
+//                 current: 'sunny',
+//                 day_val: '1',
+//                 overcast: '9'
+
+//             }
+
+            
+//         }
+//     ]
+// //     let obj = weatherData.find(o => o.condition.day_val === '1');
+
+// // console.log(obj);
+// function specificLocationData(data) {
+//     return data.location==userLocation;
+// }
+
+// const filteredData=weatherData.filter(specificLocationData);
+// console.log(filteredData[0]);
+
+
+// }
+
+// getWeatherData('Kolkata');
+
+function getWeatherData(enterLocation) {
     const weatherData =[
         {
-            location: 'kolkata',
+           
+            location:'kolkata',
             lat:'23',
             long:'12',
-            
+            temp:"25",
+            feels : "32",
+            wind:"12kmph",
+        },
+        {
+            location: 'burdwan',
+            lat:'24',
+            long:'13',
+            temp:"29",
+            feels : "31",
+            wind:"12.4kmph",
+        },
+        
 
-            
-        }
     ]
-    let obj = weatherData.find(o => o.lat === '23');
-
-console.log(obj);
+    function specificLocation(data) {
+        return data.location==enterLocation;
+    }
+    const filteredData=weatherData.filter(specificLocation);
+    console.log(filteredData[0]);
 }
-
-getWeatherData()
+getWeatherData('burdwan');
